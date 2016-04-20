104c562beb7de37c667aef39245b1ac683ed2317
Richard <richarddeng88@gmail.com>
2016-04-20 12:13:41 -0400

    1

---
+++ EDA.R
@@ -0,0 +1,5 @@
+before_co <- read.csv("data/uncommongoods/before_co.csv",stringsAsFactors = T)
+before_ugids <- read.csv("data/uncommongoods/before_co_ugids.csv",stringsAsFactors = T)
+before_wl <- read.csv("data/uncommongoods/before_wl.csv",stringsAsFactors = T)
+before_wl_ugids <- read.csv("data/uncommongoods/before_wl_ugids.csv",stringsAsFactors = T)
+
