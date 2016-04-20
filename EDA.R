5b44ea909b84a07e131ba07f6e5d74401a402b0a
Richard <richarddeng88@gmail.com>
2016-04-20 15:03:20 -0400

    2

--- EDA.R
+++ EDA.R
@@ -1,5 +1,22 @@
-before_co <- read.csv("data/uncommongoods/before_co.csv",stringsAsFactors = T)
-before_ugids <- read.csv("data/uncommongoods/before_co_ugids.csv",stringsAsFactors = T)
-before_wl <- read.csv("data/uncommongoods/before_wl.csv",stringsAsFactors = T)
-before_wl_ugids <- read.csv("data/uncommongoods/before_wl_ugids.csv",stringsAsFactors = T)
+#### READ DATA INTO R #######################
+    before_co <- read.csv("data/uncommongoods/before_co.csv",stringsAsFactors = T)
+    before_ugids <- read.csv("data/uncommongoods/before_co_ugids.csv",stringsAsFactors = T)
+    before_wl <- read.csv("data/uncommongoods/before_wl.csv",stringsAsFactors = T)
+    before_wl_ugids <- read.csv("data/uncommongoods/before_wl_ugids.csv",stringsAsFactors = T)
+    
+    after_co <- read.csv("data/uncommongoods/after_co.csv",stringsAsFactors = T)
+    after_ugids <- read.csv("data/uncommongoods/after_co_ugids.csv",stringsAsFactors = T)
+    after_wl <- read.csv("data/uncommongoods/after_wl.csv",stringsAsFactors = T)
+    after_wl_ugids <- read.csv("data/uncommongoods/after_wl_ugids.csv",stringsAsFactors = T)
+    
+    test_co <- read.csv("data/uncommongoods/test_co.csv",stringsAsFactors = T)
+    test_ugids <- read.csv("data/uncommongoods/test_co_ugids.csv",stringsAsFactors = T)
+    test_wl <- read.csv("data/uncommongoods/test_wl.csv",stringsAsFactors = T)
+    test_wl_ugids <- read.csv("data/uncommongoods/test_wl_ugids.csv",stringsAsFactors = T)
 
+#### EDA ##############################
+    
+    
+    
+    
+    
