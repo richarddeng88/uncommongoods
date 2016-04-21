9e57cf6c169ead956b26743e423c7ade2325f792
Richard <richarddeng88@gmail.com>
2016-04-21 00:52:38 -0400

    5

--- EDA.R
+++ EDA.R
@@ -6,12 +6,12 @@
     
     after_co <- read.csv("data/uncommongoods/after_co.csv",stringsAsFactors = T)
     after_ugids <- read.csv("data/uncommongoods/after_co_ugids.csv",stringsAsFactors = T)
-    after_wl <- read.csv("data/uncommongoods/after_wl.csv",stringsAsFactors = T)
+    after_wl <- read.csv("data/uncommongoods/after_wl.csv",stringsAsFactors = F)
     after_wl_ugids <- read.csv("data/uncommongoods/after_wl_ugids.csv",stringsAsFactors = T)
     
     test_co <- read.csv("data/uncommongoods/test_co.csv",stringsAsFactors = T)
     test_ugids <- read.csv("data/uncommongoods/test_co_ugids.csv",stringsAsFactors = T)
-    test_wl <- read.csv("data/uncommongoods/test_wl.csv",stringsAsFactors = T)
+    test_wl <- read.csv("data/uncommongoods/test_wl.csv",stringsAsFactors = F)
     test_wl_ugids <- read.csv("data/uncommongoods/test_wl_ugids.csv",stringsAsFactors = T)
 
 #### EDA ##############################
