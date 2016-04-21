14611059e69f1498c1401b45f4654b75c68a370e
Richard <richarddeng88@gmail.com>
2016-04-21 15:44:38 -0400

    6

--- EDA.R
+++ EDA.R
@@ -9,7 +9,7 @@
     after_wl <- read.csv("data/uncommongoods/after_wl.csv",stringsAsFactors = F)
     after_wl_ugids <- read.csv("data/uncommongoods/after_wl_ugids.csv",stringsAsFactors = T)
     
-    test_co <- read.csv("data/uncommongoods/test_co.csv",stringsAsFactors = T)
+    test_co <- read.csv("data/uncommongoods/test_co.csv",stringsAsFactors = F)
     test_ugids <- read.csv("data/uncommongoods/test_co_ugids.csv",stringsAsFactors = T)
     test_wl <- read.csv("data/uncommongoods/test_wl.csv",stringsAsFactors = F)
     test_wl_ugids <- read.csv("data/uncommongoods/test_wl_ugids.csv",stringsAsFactors = T)
@@ -57,10 +57,6 @@
         #}
         
     # work on wish list data
-        # research on the login_or_creation values. 
-        prop.table(table(before_wl$reg_success))
-        prop.table(table(test_wl$reg_success))
-        prop.table(table(after_wl$reg_success))
         
         # FB VS GOOGLE. FB looks better than google. 
         table(test_wl$themodule)
@@ -73,60 +69,172 @@
         avg1 <- summarize(a1, avg = mean(eventcount))
         avg2 <- summarize(a2, avg = mean(eventcount))
         avg3 <- summarize(a3, avg = mean(eventcount))
-    
-        table(before_wl$eventcount)
+
        
         # user by session
         length(unique(before_wl$eventcount))
         length(unique(before_wl$isloggedin_r))
         length(unique(before_wl$login_or_create))
         
-        a <- before_wl[, c(2,6,8)]
-        b <- grep("C", a$login_or_create)
-        d <- grep("L", a$login_or_create)
-        dim(a[b,]) # 877 try to creat a user
-        dim(a[d,]) # 812 try to login in. 
-        a$isloggedin_r <- gsub("user by-session", "no", a$isloggedin_r)
-        c <- grep("user", a$isloggedin_r)
-        dim(a[c,]) #767, sucessfully login in 
+        # find out how many user sucessfuly login either by C or BY L
+        #a <- before_wl[, c(2,6,8)]
+        #b <- grep("C", a$login_or_create)
+        #d <- grep("L", a$login_or_create)
+        #dim(a[b,]) # 877 try to creat a user
+        #dim(a[d,]) # 812 try to login in. 
+        before_wl$isloggedin_r <- gsub("user by-session", "no", before_wl$isloggedin_r) 
+        before_success <- grep("user", before_wl$isloggedin_r)
+        dim(before_wl[before_success,]) #767, sucessfully login in 
+                
+                # find out how many successfully create a user account , and how many log in using existing account
+                s <- before_wl[before_success,]
+                s$new <- sub("user","login",s$isloggedin_r)
+                position <- rep(0,dim(s)[1])
+                for (i in 1:dim(s)[1]){
+                    s1 <-unlist(strsplit(s$new[i], split = ","))
+                    position[i] <- which(s1=="login")-1
+                }
+                
+                c_l <- rep(NA, dim(s)[1])
+                for (i in 1:dim(s)[1]){
+                    s1 <-unlist(strsplit(s$login_or_create[i], split = ","))
+                    c_l[i] <- s1[position[i]]
+                }
+                table(c_l) # 508 are C, 259 are L, totally 767
+                
+                # add a new variable named "reg_success: 1 if user created an account, else 0"
+                before_wl$user_login <- "no"
+                before_wl[before_success,]$user_login <- c_l
+                before_wl$reg_success <- 0
+                before_wl$reg_success[before_wl$user_login=="C"] <- 1
+                prop.table(table(before_wl$reg_success))
+                
+                
+                
+        #a1 <- test_wl[,c(2,6,8)]
+        #b1 <- grep("C", a1$login_or_create)
+        #d1 <- grep("L", a1$login_or_create)
+        #dim(a1[b1,]) # 832 try to creat a user
+        #dim(a1[d1,]) # 761 try to login in. 
+        test_wl$isloggedin_r <- gsub("user by-session", "no", test_wl$isloggedin_r)
+        test_success <- grep("user", test_wl$isloggedin_r)
+        dim(test_wl[test_success,]) #740, sucessfully login in 
+                
+                ##test 
+                s <- test_wl[test_success,]
+                s$new <- sub("user","login",s$isloggedin_r)
+                position <- rep(0,dim(s)[1])
+                for (i in 1:dim(s)[1]){
+                    s1 <-unlist(strsplit(s$new[i], split = ","))
+                    position[i] <- which(s1=="login")-1
+                }
+                
+                c_l <- rep(NA, dim(s)[1])
+                for (i in 1:dim(s)[1]){
+                    s1 <- unlist(strsplit(s$login_or_create[i], split = ","))
+                    c_l[i] <- s1[position[i]]
+                }
+                table(c_l) # 527 are C, 213 are L, totally 740
+                
+                # add a new variable named "reg_success: 1 if user created an account, else 0"
+                test_wl$user_login <- "no"
+                test_wl[test_success,]$user_login <- c_l
+                test_wl$reg_success <- 0
+                test_wl$reg_success[test_wl$user_login=="C"] <- 1
+                prop.table(table(test_wl$reg_success))
         
-        a1 <- test_wl[,c(2,6,8)]
-        b1 <- grep("C", a1$login_or_create)
-        d1 <- grep("L", a1$login_or_create)
-        dim(a1[b1,]) # 832 try to creat a user
-        dim(a1[d1,]) # 761 try to login in. 
-        a1$isloggedin_r <- gsub("user by-session", "no", a1$isloggedin_r)
-        c1 <- grep("user", a1$isloggedin_r)
-        dim(a1[c1,]) #740, sucessfully login in 
+        #a2 <- after_wl[,c(2,6,8)]
+        #b2 <- grep("C", a2$login_or_create)
+        #d2 <- grep("L", a2$login_or_create)
+        #dim(a2[b2,]) # 117 try to creat a user
+        #dim(a2[d2,]) # 212 try to login in. 
+        after_wl$isloggedin_r <- gsub("user by-session", "no", after_wl$isloggedin_r)
+        after_success <- grep("user", after_wl$isloggedin_r)
+        dim(after_wl[after_success,]) #176, sucessfully login in 
         
-        a2 <- after_wl[,c(2,6,8)]
-        b2 <- grep("C", a2$login_or_create)
-        d2 <- grep("L", a2$login_or_create)
-        dim(a2[b2,]) # 117 try to creat a user
-        dim(a2[d2,]) # 212 try to login in. 
-        a2$isloggedin_r <- gsub("user by-session", "no", a2$isloggedin_r)
-        c2 <- grep("user", a2$isloggedin_r)
-        dim(a2[c2,]) #176, sucessfully login in 
+                # after
+                s <- after_wl[after_success,]
+                s$new <- sub("user","login",s$isloggedin_r)
+                position <- rep(0,dim(s)[1])
+                for (i in 1:dim(s)[1]){
+                    s1 <-unlist(strsplit(s$new[i], split = ","))
+                    position[i] <- which(s1=="login")-1
+                }
+                
+                c_l <- rep(NA, dim(s)[1])
+                for (i in 1:dim(s)[1]){
+                    s1 <- unlist(strsplit(s$login_or_create[i], split = ","))
+                    c_l[i] <- s1[position[i]]
+                }
+                table(c_l) # 527 are C, 213 are L, totally 740
         
-        
-        # how to identify a user sucessfuly create a new account. 
-        # C,N,N;
-
-        s <- a[c,]
-        s$new <- sub("user","login",s$isloggedin_r)
-        position <- rep(0,dim(s)[1])
-        for (i in 1:dim(s)[1]){
-        s1 <-unlist(strsplit(s$new[i], split = ","))
-        position[i] <- which(s1=="login")-1
-        }
-        
-        c_l <- rep(NA, dim(s)[1])
-        for (i in 1:dim(s)[1]){
-            s1 <-unlist(strsplit(s$login_or_create[i], split = ","))
-            c_l[i] <- s1[position[i]]
-        }
-        table(c_l)
-        
-        
+                # add a new variable named "reg_success: 1 if user created an account, else 0"
+                after_wl$user_login <- "no"
+                after_wl[after_success,]$user_login <- c_l
+                after_wl$reg_success <- 0
+                after_wl$reg_success[after_wl$user_login=="C"] <- 1
+                prop.table(table(after_wl$reg_success))
+                
         # DATA VISUALIZATION
-        
+            # Q1: impact on the creation of new users - WL and CO , bar chart
+            percent <- c(mean(before_wl$reg_success),mean(test_wl$reg_success),mean(after_wl$reg_success),
+                    mean(before_co$reg_success),mean(test_co$reg_success),mean(after_co$reg_success))
+            
+            impact <- data.frame(percent = percent, time=c("1.before","2.test","3.after","1.before","2.test","3.after"),
+                            type= c("WL","WL","WL","CO","CO","CO"))
+            
+            library(ggplot2)
+            ggplot(impact, aes(x=time,y=percent,fill=time)) + geom_bar(stat="identity",position="dodge", colour="black")+
+                facet_grid(.~type)+scale_fill_manual(values=c("#669933", "#FFCC66","#669933"))+
+                xlab("Time Period") + ylab("Sucessful Account Creation Percent")
+            
+            # Q3: FB VS GOOGLE, PIE chart
+            
+            table(test_co$themodule);table(test_co$reg_success)
+            table(test_wl$themodule);table(test_wl$reg_success)
+            
+            # an first glance : the click caomparison of FB vs GOOGLE, 
+            #but many of them are unsucessfull creating a new account. 
+            f1 <- test_co[test_co$themodule!=0,]$themodule
+            f2 <- test_wl[test_wl$themodule!=0,]$themodule
+                
+                # creating pie chart. 
+                # CO 
+                slices <- c(table(f1)[1],table(f1)[2])
+                lbls <- c("FB", "GOOGLE")
+                # pct <- round(slices/sum(slices)*100)
+                lbls <- paste(lbls, slices) 
+                #lbls <- paste(lbls,"%",sep="")
+                pie(slices, labels = lbls, col=c("blue","lightgreen"),main="CO - Pie Chart of clikc")
+                # WL
+                slices <- c(table(f2)[1],table(f2)[2])
+                lbls <- c("FB", "GOOGLE")
+                lbls <- paste(lbls, slices) 
+                pie(slices, labels = lbls, col=c("blue","lightgreen"),main="WL - Pie Chart of clikc")
+                
+            #with in these successful account creation, the GB VS GOOGLE number are different. 
+            f1 <- test_co[test_co$reg_success==1,]$themodule
+            f2 <- test_wl[test_wl$reg_success==1,]$themodule
+            #f1 <- as.character(f1); 
+            f1[f1=="0"] <- "email"; 
+            #f1 <- as.factor(f1)
+            #f2 <- as.character(f2); 
+            f2[f2=="0"] <- "email"; 
+            #f2 <- as.factor(f2)
+                
+            # creating pie chrt
+                #CO
+                slices <- c(table(f1)[1],table(f1)[2],table(f1)[3])
+                lbls <- c("email","FB", "GOOGLE")
+                # pct <- round(slices/sum(slices)*100)
+                lbls <- paste(lbls, slices) 
+                #lbls <- paste(lbls,"%",sep="")
+                pie(slices, labels = lbls, col=rainbow(length(lbls)),main="CO - successful account creation")
+                # WL
+                slices <- c(table(f2)[1],table(f2)[2],table(f2)[3])
+                lbls <- c("email","FB", "GOOGLE")
+                lbls <- paste(lbls, slices) 
+                pie(slices, labels = lbls, col=rainbow(length(lbls)),main="WL - successful account creation")
+                
+
+       
