bc3d7cd32ac8ff167359f170af92bb749313188b
Richard <richarddeng88@gmail.com>
2016-04-21 00:06:16 -0400

    4

--- EDA.R
+++ EDA.R
@@ -1,7 +1,7 @@
 #### READ DATA INTO R #######################
     before_co <- read.csv("data/uncommongoods/before_co.csv",stringsAsFactors = T)
     before_ugids <- read.csv("data/uncommongoods/before_co_ugids.csv",stringsAsFactors = T)
-    before_wl <- read.csv("data/uncommongoods/before_wl.csv",stringsAsFactors = T)
+    before_wl <- read.csv("data/uncommongoods/before_wl.csv",stringsAsFactors = F)
     before_wl_ugids <- read.csv("data/uncommongoods/before_wl_ugids.csv",stringsAsFactors = T)
     
     after_co <- read.csv("data/uncommongoods/after_co.csv",stringsAsFactors = T)
@@ -21,13 +21,15 @@
     sum(is.na(after_co));sum(is.na(after_wl));sum(is.na(after_ugids));sum(is.na(after_wl_ugids))
     
     # work on check out data
-        #login? it turns out social login actually didn't encourage user to create login in account. 
+        #login? it turns out social login actually didn't encourage user to create login in account, loing rate decrease.  
         prop.table(table(before_co$reg_success))
         prop.table(table(test_co$reg_success))
         prop.table(table(after_co$reg_success))
         
-        # FACEBOOK VS GOOGLE, FB has more users login creation. 
+        # FACEBOOK VS GOOGLE, FB has more users login creation. 1040 create new account, from which 191 use social. 
         prop.table(table(test_co$themodule))
+        table(test_co$themodule)
+        table(test_co$reg_success)
         
         # eventcount
         prop.table(table(before_co$eventcount))
@@ -72,4 +74,59 @@
         avg2 <- summarize(a2, avg = mean(eventcount))
         avg3 <- summarize(a3, avg = mean(eventcount))
     
-    
+        table(before_wl$eventcount)
+       
+        # user by session
+        length(unique(before_wl$eventcount))
+        length(unique(before_wl$isloggedin_r))
+        length(unique(before_wl$login_or_create))
+        
+        a <- before_wl[, c(2,6,8)]
+        b <- grep("C", a$login_or_create)
+        d <- grep("L", a$login_or_create)
+        dim(a[b,]) # 877 try to creat a user
+        dim(a[d,]) # 812 try to login in. 
+        a$isloggedin_r <- gsub("user by-session", "no", a$isloggedin_r)
+        c <- grep("user", a$isloggedin_r)
+        dim(a[c,]) #767, sucessfully login in 
+        
+        a1 <- test_wl[,c(2,6,8)]
+        b1 <- grep("C", a1$login_or_create)
+        d1 <- grep("L", a1$login_or_create)
+        dim(a1[b1,]) # 832 try to creat a user
+        dim(a1[d1,]) # 761 try to login in. 
+        a1$isloggedin_r <- gsub("user by-session", "no", a1$isloggedin_r)
+        c1 <- grep("user", a1$isloggedin_r)
+        dim(a1[c1,]) #740, sucessfully login in 
+        
+        a2 <- after_wl[,c(2,6,8)]
+        b2 <- grep("C", a2$login_or_create)
+        d2 <- grep("L", a2$login_or_create)
+        dim(a2[b2,]) # 117 try to creat a user
+        dim(a2[d2,]) # 212 try to login in. 
+        a2$isloggedin_r <- gsub("user by-session", "no", a2$isloggedin_r)
+        c2 <- grep("user", a2$isloggedin_r)
+        dim(a2[c2,]) #176, sucessfully login in 
+        
+        
+        # how to identify a user sucessfuly create a new account. 
+        # C,N,N;
+
+        s <- a[c,]
+        s$new <- sub("user","login",s$isloggedin_r)
+        position <- rep(0,dim(s)[1])
+        for (i in 1:dim(s)[1]){
+        s1 <-unlist(strsplit(s$new[i], split = ","))
+        position[i] <- which(s1=="login")-1
+        }
+        
+        c_l <- rep(NA, dim(s)[1])
+        for (i in 1:dim(s)[1]){
+            s1 <-unlist(strsplit(s$login_or_create[i], split = ","))
+            c_l[i] <- s1[position[i]]
+        }
+        table(c_l)
+        
+        
+        # DATA VISUALIZATION
+        
